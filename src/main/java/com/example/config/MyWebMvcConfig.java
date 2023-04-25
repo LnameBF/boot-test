faaa42956a5fe69e9cf6c72eb9129c4e62896c9c
Hei <waityou32505@163.com>
2023-04-25 10:27:31 +0800

    资源映射

---
+++ src/main/java/com/example/config/MyWebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.example.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 静态资源映射
+ */
+@Component
+public class MyWebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //这样配置表示浏览器中携带static的所有请求都会映射道resources目录下的static目录
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/static/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/static/css/**").addResourceLocations("classpath:/static/css/");
+    }
+}
