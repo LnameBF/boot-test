a8cd9df21c8ed56e84fc10c860084e0ccc9d03c3
Hei <waityou32505@163.com>
2023-04-25 12:06:02 +0800

    资源映射

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -1,8 +1,14 @@
 package com.example.controller;
 
+import com.example.dao.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
@@ -16,8 +22,20 @@ public class IndexController {
         return "login";
     }
 
+    /**
+     * 登录成功后重定向道main页面 ，防止表单重复提交
+     * @return
+     */
     @PostMapping("/login")
-    public String main(String username, String password) {
+    public String main(@RequestBody User user, HttpSession session) {
+        System.out.println(user.getUsername());
+        return "redirect:/main.html";
+    }
+
+    @GetMapping("/main.html")
+    public String gotoPage(){
+
+
         return "main";
     }
 
